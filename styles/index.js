0a539e331b41d083dce19ce124bbaa8691c78b41
Marko Mijakovski <m.mijakovski07@gmail.com>
2024-09-18 14:58:50 +0200

    .

--- styles/index.js
+++ styles/index.js
@@ -6,15 +6,17 @@ const dropContent = document.querySelector('#dropdown-menue');
 const burgerBtn = document.querySelector('#burgerMenu');
 let burgerSvg = document.querySelectorAll('#burgerMenu svg');
 const navBar = document.querySelector('#navbar');
+const darkBack_BurgerClick = document.querySelector('#dark-background');
 let arrowRotation = 0;
 let prevWidth = 0;
 
 //Variables for carousels
 const topContent = document.querySelectorAll('#top-carousel-content');
-console.log(topContent);
-let nav = 0;
-let spacing = 1, times = 1;
+const Content_Parent = document.querySelector('#content-parent');
 
+let nav = 0, dragging = false, dragX;
+let spacing = 1, times = 1;
+let timer;
 
 
 //Functions
@@ -34,11 +36,15 @@ const Check_Burger_Status = (e) => {
    if(e[1].style.display === 'none') {
     e[1].style.display = 'block';
     e[0].style.display = 'none';
-    
+
+    document.querySelector('body').style.overflow = 'hidden';
+    Check_Active_State(darkBack_BurgerClick);
    } else {
     e[1].style.display = 'none';
     e[0].style.display = 'block';
 
+    document.querySelector('body').style.overflow = 'auto';
+    Check_Active_State(darkBack_BurgerClick);
    }
 };
 
@@ -81,8 +87,19 @@ const responsive = (minWidth) => {
 
     if(minWidth <= 1199) {
         document.querySelector('#dropdownBtn > span').addEventListener('click', () => {Check_Active_State(dropContent); rotateArrow()});
+
+        //This is code for the darkBack_BurgerClick
+        if(burgerSvg[1].style.display === 'block') {
+            darkBack_BurgerClick.classList.add('active');
+        } else {
+            darkBack_BurgerClick.classList.remove('active');
+        }
+
     } else {
         dropBtn.addEventListener('click', () => {Check_Active_State(dropContent)});
+        
+        //This is code for the darkBack_BurgerClick
+        darkBack_BurgerClick.classList.remove('active');
     }
 };
 
@@ -93,11 +110,46 @@ const Sliding = (car, indx) => {
     for(let i = 0; i < car.length; i++) {
         if(indx === i) {
             car[i].style.opacity = '100%';
+            car[i].style.display = 'block';
         } else {
             car[i].style.opacity = '0%';
+            car[i].style.display = 'none';
         }
     }
 };
+
+const outOfRange = () => {
+   console.log(nav);
+
+   if(nav < 0) {
+        nav = (topContent.length - 1);
+    } else if(nav > (topContent.length - 1)) {
+        nav = 0;
+   }
+   
+};
+
+const startDragging = (e) => {
+    dragging = true;
+    dragX = e.pageX;
+    Dragging();
+};
+
+const Dragging = () => {
+    if(dragging) {
+       Content_Parent.classList.add('dragg'); 
+    } else {
+        Content_Parent.classList.remove('dragg');
+    }
+};
+
+const stopDragging = () => {
+    dragging = false;
+    Dragging();
+};
+
+
+
 
 
 //Calling Functions
@@ -109,15 +161,24 @@ burgerBtn.addEventListener('click', () =>{Check_Active_State(navBar); Check_Burg
 
 //Carousels
 
+Sliding(topContent, nav);
+
+Content_Parent.addEventListener('mouseover', startDragging);
+Content_Parent.addEventListener('mouseleave', stopDragging);
+
 document.querySelector('#prevBtn').addEventListener('click', function() {
     nav = nav - 1;
-    Sliding(topContent,nav);
+
+    outOfRange();
+    Sliding(topContent, nav);
 });
 
 document.querySelector('#nextBtn').addEventListener('click', function() {
     nav = nav + 1;
-    Sliding(topContent,nav);
+    
+    outOfRange();
+    Sliding(topContent, nav);
 });
- 
+
   
  
