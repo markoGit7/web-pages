d4daee2cd3fb6fc91f667a821a2bafae34531267
Marko Mijakovski <m.mijakovski07@gmail.com>
2024-09-24 13:24:18 +0200

    .

--- styles/animations.js
+++ styles/animations.js
@@ -2,21 +2,3 @@ AOS.init({
     once: true,
 });
 
-//Global Variables
-const servicesContent = document.querySelectorAll('#services');
-let delay = 0;
-//Functions
-const setDelay = (number,content) => {
-    
-    content.setAttribute('data-aos-delay', number);  
-    
-};
-
-//Calling Functions
-
-if(window.innerWidth > 767) {
-    for(let i = 0; i < servicesContent.length; i++) {
-        setDelay(delay,servicesContent[i]);
-        delay = delay + 250;
-    }
-}
