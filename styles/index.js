a2a422299cd7ae9adb3ecbe3ee5b930847a12575
Marko Mijakovski <m.mijakovski07@gmail.com>
2024-11-30 13:45:14 +0100

    .

--- styles/index.js
+++ styles/index.js
@@ -542,24 +542,5 @@ window.onscroll = () => {
 
 
 /* portfolio-single.html */
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
