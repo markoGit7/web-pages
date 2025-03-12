5839cec7c650cbaa2404507182716769757b7fc5
Marko Mijakovski <m.mijakovski07@gmail.com>
2025-03-12 10:56:57 +0100

    react project started

---
+++ create-binary/src/Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+
+    return (
+        <Footer>
+            <div className="boxed">
+                <div className="internal">
+                    <p>&copy;All rights reserved</p>
+                </div>
+            </div>
+        </Footer>
+    );
+}
+
+export default Footer;
---
+++ create-binary/src/Header.jsx
@@ -0,0 +1,28 @@
+
+function Header() {
+
+    return (
+        <header>
+            <div className="boxed">
+                <div className="internal">
+                    <h1>Logo</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
